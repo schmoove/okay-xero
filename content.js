cebe372b7de27e5406beccad3fa7bb111f81cf81
schmoove <ryan@schmoove.co.nz>
2024-03-23 22:44:19 +1300

    WIP - add support for Esc/Cancel, remove jQuery dependency

--- content.js
+++ content.js
@@ -1,11 +1,18 @@
-$(".statement input").focus(function() {
-    $(".statement input").keydown(function (event) {
-        if ( ( event.metaKey || event.ctrlKey ) && event.keyCode == 13 ) {
-            var $ok = $(this).closest('[data-statementlineid]').find('.okayButton');
-            if ( $ok.css('visibility') == 'visible' ) {
-                let e = new Event("click");
-                $ok.get(0).dispatchEvent(e);
+$('.statement input').forEach(function(inputField) {
+    inputField.addEventListener('focus', function(focusEvent) {
+        focusEvent.currentTarget.addEventListener('keydown', function(keydownEvent) {
+            // OK
+            if ( ( keydownEvent.metaKey || keydownEvent.ctrlKey ) && keydownEvent.keyCode == 13 ) {
+                var $ok = keydownEvent.currentTarget.closest('[data-statementlineid]').find('.okayButton');
+                if ( $ok.style.visibility == 'visible' ) {
+                    let e = new Event("click");
+                    //$ok.get(0).dispatchEvent(e);
+                }
             }
-        }
-    });
+            // Cancel
+            if ( keydownEvent.keyCode == 27 ) {
+
+            }
+        });
+    }
 });
