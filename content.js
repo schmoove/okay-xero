bffab3ebf4b62dae838cf2290e0919fa91d3b1c8
schmoove <ryan@schmoove.co.nz>
2025-11-15 15:07:00 +1300

    Upgrade to v3 manifest, remove jQuery dependency

--- content.js
+++ content.js
@@ -1,18 +1,13 @@
-$('.statement input').forEach(function(inputField) {
+document.querySelectorAll('.statement input').forEach(function(inputField) {
     inputField.addEventListener('focus', function(focusEvent) {
         focusEvent.currentTarget.addEventListener('keydown', function(keydownEvent) {
-            // OK
-            if ( ( keydownEvent.metaKey || keydownEvent.ctrlKey ) && keydownEvent.keyCode == 13 ) {
-                var $ok = keydownEvent.currentTarget.closest('[data-statementlineid]').find('.okayButton');
-                if ( $ok.style.visibility == 'visible' ) {
-                    let e = new Event("click");
-                    //$ok.get(0).dispatchEvent(e);
+            if ( ( keydownEvent.metaKey || keydownEvent.ctrlKey ) && keydownEvent.keyCode === 13 ) {
+                const okButton = keydownEvent.currentTarget.closest('.line')?.querySelector('.okayButton');
+                if ( okButton && getComputedStyle(okButton).visibility === 'visible' ) {
+                    const e = new Event("click");
+                    okButton.dispatchEvent(e);
                 }
             }
-            // Cancel
-            if ( keydownEvent.keyCode == 27 ) {
-
-            }
         });
-    }
+    });
 });
