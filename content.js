5247fdb44545cbd1aefa5a2d1f6dc25911d72bce
schmoove <ryan@schmoove.co.nz>
2025-11-15 15:40:19 +1300

    Use dynamic event listener

--- content.js
+++ content.js
@@ -1,13 +1,45 @@
-document.querySelectorAll('.statement input').forEach(function(inputField) {
-    inputField.addEventListener('focus', function(focusEvent) {
-        focusEvent.currentTarget.addEventListener('keydown', function(keydownEvent) {
-            if ( ( keydownEvent.metaKey || keydownEvent.ctrlKey ) && keydownEvent.keyCode === 13 ) {
-                const okButton = keydownEvent.currentTarget.closest('.line')?.querySelector('.okayButton');
-                if ( okButton && getComputedStyle(okButton).visibility === 'visible' ) {
-                    const e = new Event("click");
-                    okButton.dispatchEvent(e);
-                }
-            }
-        });
-    });
-});
+// https://github.com/Cristy94/dynamic-listener
+!(function (e) {
+  "use strict";
+  e.addDynamicEventListener = function (e, t, r, n, a) {
+    var i = (function (e, t) {
+      return function (r) {
+        if (r.target && r.target.matches(e))
+          return (r.delegatedTarget = r.target), void t.apply(this, arguments);
+        var n = event.path || (event.composedPath && event.composedPath());
+        if (n)
+          for (var a = 0; a < n.length; ++a) {
+            var i = n[a];
+            if (
+              (i.matches(e) &&
+                ((r.delegatedTarget = i), t.apply(this, arguments)),
+              i === r.currentTarget)
+            )
+              return;
+          }
+      };
+    })(r, n);
+    return e.addEventListener(t, i, a), e.removeEventListener.bind(e, t, i, a);
+  };
+})(this);
+
+addDynamicEventListener(
+  document.body,
+  "keydown",
+  '.create input[type="text"]',
+  function (e) {
+    console.log("keydown", e.target.innerText);
+    if ((e.metaKey || e.ctrlKey) && e.keyCode === 13) {
+      const okButton = e.delegatedTarget
+        .closest(".line")
+        .querySelector(".okayButton");
+      if (okButton && getComputedStyle(okButton).visibility === "visible") {
+        const e = new Event("click");
+        okButton.dispatchEvent(e);
+        console.log("[okayXero]", "OK!");
+      } else {
+        console.log("[okayXero]", "OK button not found or not visible");
+      }
+    }
+  }
+);
